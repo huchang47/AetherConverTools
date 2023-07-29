f0a4b359dd318b1538d353a5b25b4413719d8e2f
huchang47 <huchang47@hotmail.com>
2023-07-30 01:50:43 +0800

    把tagger整合进第一步，同时删除不再使用的最大裁切

--- bin/Stage1.py
+++ bin/Stage1.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 import shutil
+import subprocess
 from torchvision import transforms
 from PIL import Image
 
@@ -118,182 +119,9 @@ for file_name in png_files:
     
 print(f"原始坐标已保存至 {output_file_path}")
 
-# 生成坐标文件
-width = int(width)
-height = int(height)
-
-# 步骤一：创建备用1.txt文件，进行第一轮转换
-print("正在执行步骤一：新建备用1.txt文件...")
-
-if os.path.exists("备用1.txt"):
-    os.remove("备用1.txt")
-with open("备用1.txt", "w") as f:
-    pass
-
-# 步骤二：计算坐标差值和平均值，并将结果写入备用1.txt
-print("正在执行步骤二：计算坐标差值和平均值...")
-with open(output_file_path, "r") as f:
-    lines = f.readlines()
-
-count = 0
-for line in lines:
-    print("正在处理行:", line.strip())  # 输出行内容
-    filename,x1,y1,x2,y2 = map(str,line.split(','))
-    x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
-
-    diff_x = x2 - x1
-    diff_y = y2 - y1
-    avg_x = (x1 + x2) // 2
-    avg_y = (y1 + y2) // 2
-
-    with open("备用1.txt", "a") as f:
-        new_line = f"{filename},{diff_x},{diff_y},{avg_x},{avg_y}\n"
-        f.write(new_line)
-
-    count += 1
-
-print(f"已处理 {count} 行数据，并将结果写入备用1.txt。")
-
-# 步骤三：新建备用2.txt文件
-print("正在执行步骤四：新建备用2.txt文件...")
-
-if os.path.exists("备用2.txt"):
-    os.remove("备用2.txt")
-
-with open("备用2.txt", "w") as f:
-    pass
-
-# 步骤四：借助备用1.txt的信息生成备用2.txt
-print("步骤四：借助备用1.txt的信息生成备用2.txt")
-
-max_x_diff = 0
-max_y_diff = 0
-
-with open("备用1.txt", "r") as f:
-    lines = f.readlines()
-
-for line in lines:
-
-    filename,x1,y1,x2,y2 = map(str,line.split(','))
-
-    x_diff = int(x1)
-    y_diff = int(y1)
-
-    if x_diff > max_x_diff:
-        max_x_diff = x_diff
-
-    if y_diff > max_y_diff:
-        max_y_diff = y_diff
-
-print("最大X坐标差值:", max_x_diff)
-print("最大Y坐标差值:", max_y_diff)
-
-piancha_x = 0
-piancha_y = 0
-
-if max_x_diff > width:
-	max_x_diff = width
-	piancha_x = 1
-	print("尺寸过小，为了程序运行，已修正X数值")
-	print("最大X坐标差值:", max_x_diff)
-if max_y_diff > height:
-	max_y_diff = height
-	piancha_y = 1
-	print("尺寸过小，为了程序运行，已修正Y数值")
-	print("最大Y坐标差值:", max_y_diff)
-
-with open("备用2.txt", "w") as f:
-    for line in lines:
-        filename,x1,y1,x2,y2 = map(str,line.split(','))
-        x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
-
-        new_x1 = x2 - round(max_x_diff / 2, 1)
-        new_y1 = y2 - round(max_y_diff / 2, 1)
-        new_x2 = x2 + round(max_x_diff / 2, 1)
-        new_y2 = y2 + round(max_y_diff / 2, 1)
-        new_line = f"{filename},{new_x1:.1f},{new_y1:.1f},{new_x2:.1f},{new_y2:.1f}\n"
-        f.write(new_line)
-        
-		
-        print("x1值:", x1)
-        print("y1值:", y1)
-        print("x2值:", x2)
-        print("y2值:", y2)
-
-# 步骤五：新建坐标.txt文件
-print("正在执行步骤五：新建改造坐标.txt文件...")
-
-if os.path.exists("改造坐标.txt"):
-    os.remove("改造坐标.txt")
-
-with open("改造坐标.txt", "w") as f:
-    pass
-
-# 步骤六：借助备用2.txt的信息生成改造坐标.txt
-print("步骤六：借助备用2.txt的信息生成改造改造坐标.txt...")
-with open("备用2.txt", "r") as f:
-    lines = f.readlines()
-
-for line in lines:
-    filename,x1,y1,x2,y2 = map(str,line.split(','))
-    x1, y1, x2, y2 = float(x1),float(y1),float(x2),float(y2)
-
-    if piancha_x == 0:
-        if x1 < 0:
-            p = 0 - x1
-            x1 = x1 + p
-            x2 = x2 + p
-
-        if x2 > float(width):
-            p = x2 - float(width)
-            x2 = x2 - p
-            x1 = x1 - p
-
-    if piancha_y == 0:
-        if y1 < 0:
-            q = 0 - y1
-            y1 = y1 + q
-            y2 = y2 + q
-
-        if y2 > float(height):
-            q = y2 - float(height)
-            y2 = y2 - q
-            y1 = y1 - q
-
-    with open("改造坐标.txt", "a") as f:
-        filename = os.path.splitext(filename)[0]
-        new_line = f"{filename},{int(x1)},{int(y1)},{int(x2)},{int(y2)}\n"
-        f.write(new_line)
-
-print("第六步完成，改造坐标.txt文件生成完毕。")
-
-os.remove("备用1.txt")
-os.remove("备用2.txt")
-#os.remove(output_file_path)
-
-print("\n\n\n请选择图像裁切方式：")
-print("1. 原始坐标（按照蒙版原始大小裁切，每张图尺寸不同）")
-print("2. 最大坐标（按照最大蒙版大小裁切，每张图相同尺寸）")
-
-choice = input("请输入裁切方式的编号：")
-
-if choice == '1':
-    print("你选择了原始坐标裁切")
-    map_file = "原始坐标.txt"
-elif choice == '2':
-    print("你选择了最大坐标裁切")
-    map_file = "改造坐标.txt"
-else:
-    print("无效的选择，默认采用原始坐标方式裁切")
-    map_file = "原始坐标.txt"
-
-# 把选择结果存起来，第三步方便调用
-with open(map_file, "a") as f:
-    f.write("Choose me")
-
 # 坐标文件路径
 folder_path = os.path.dirname(os.getcwd())
-info_file_path = os.path.join(folder_path,"bin",map_file)
+info_file_path = os.path.join(folder_path,"bin","原始坐标.txt")
 
 # 检查坐标文件是否存在
 if not os.path.isfile(info_file_path):
@@ -336,4 +164,8 @@ for file, line in zip(os.listdir(mask_path), lines):
         cropped_img.save(os.path.join(mask_out_folder_path, file))
         print("蒙版"+file+"裁切完成！")
 
-print("所有帧和蒙版均裁切完毕，第一步完成！\n请进行图生图！")
+Choice=input("\n\n\n是否开始反推提示词？\n1. 是\n2. 否\n请输入你选择的编号：")
+
+subprocess.run(["python", "Stage1.5.py"])  # 执行Stage1.5.py文件
+
+print("当前步骤操作完毕，第一步完成！\n请进行第二步图生图！")

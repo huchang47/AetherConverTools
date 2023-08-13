886059b02e285ab922c13101b42ea88ff27cb7ae
huchang47 <huchang47@hotmail.com>
2023-08-14 03:33:17 +0800

    提取关键字另一个方式

---
+++ bin/04_GeneratePrompt - 2.py
@@ -0,0 +1,260 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+
+from typing import Tuple, List, Dict
+from io import BytesIO
+import cv2
+from PIL import Image
+
+from pathlib import Path
+from huggingface_hub import hf_hub_download
+
+
+def make_square(img, target_size):
+    old_size = img.shape[:2]
+    desired_size = max(old_size)
+    desired_size = max(desired_size, target_size)
+
+    delta_w = desired_size - old_size[1]
+    delta_h = desired_size - old_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    color = [255, 255, 255]
+    new_im = cv2.copyMakeBorder(
+        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+    )
+    return new_im
+
+
+def smart_resize(img, size):
+    # Assumes the image has already gone through make_square
+    if img.shape[0] > size:
+        img = cv2.resize(img, (size, size), interpolation=cv2.INTER_AREA)
+    elif img.shape[0] < size:
+        img = cv2.resize(img, (size, size), interpolation=cv2.INTER_CUBIC)
+    return img
+
+# 检查是否有可用的CUDA设备
+if torch.cuda.is_available():
+    tf_device_name = '/gpu:0'
+    print("加速成功！使用的设备：CUDA")
+else:
+    tf_device_name = '/cpu:0'
+    print("加速失败！使用的设备：CPU")
+
+# select a device to process
+use_cpu = False
+
+if use_cpu:
+    tf_device_name = '/cpu:0'
+else:
+    tf_device_name = '/gpu:0'
+
+class Interrogator:
+    @staticmethod
+    def postprocess_tags(
+        tags: Dict[str, float],
+
+        threshold=0.25,
+        additional_tags: List[str] = [],
+        exclude_tags: List[str] = [],
+        sort_by_alphabetical_order=False,
+        add_confident_as_weight=False,
+        replace_underscore=False,
+        replace_underscore_excludes: List[str] = [],
+        escape_tag=False
+    ) -> Dict[str, float]:
+        for t in additional_tags:
+            tags[t] = 1.0
+
+        # those lines are totally not "pythonic" but looks better to me
+        tags = {
+            t: c
+
+            # sort by tag name or confident
+            for t, c in sorted(
+                tags.items(),
+                key=lambda i: i[0 if sort_by_alphabetical_order else 1],
+                reverse=not sort_by_alphabetical_order
+            )
+
+            # filter tags
+            if (
+                c >= threshold
+                and t not in exclude_tags
+            )
+        }
+
+        new_tags = []
+        for tag in list(tags):
+            new_tag = tag
+
+            if replace_underscore and tag not in replace_underscore_excludes:
+                new_tag = new_tag.replace('_', ' ')
+
+            """
+            if escape_tag:
+                new_tag = tag_escape_pattern.sub(r'\\\1', new_tag)
+            """
+            if add_confident_as_weight:
+                new_tag = f'({new_tag}:{tags[tag]})'
+
+            new_tags.append((new_tag, tags[tag]))
+        tags = dict(new_tags)
+
+        return tags
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def load(self):
+        raise NotImplementedError()
+
+    def unload(self) -> bool:
+        unloaded = False
+
+        if hasattr(self, 'model') and self.model is not None:
+            del self.model
+            unloaded = True
+            print(f'Unloaded {self.name}')
+
+        if hasattr(self, 'tags'):
+            del self.tags
+
+        return unloaded
+
+    def interrogate(
+        self,
+        image: Image
+    ) -> Tuple[
+        Dict[str, float],  # rating confidents
+        Dict[str, float]  # tag confidents
+    ]:
+        raise NotImplementedError()
+
+
+class WaifuDiffusionInterrogator(Interrogator):
+    def __init__(
+        self,
+        name: str,
+        model_path='model.onnx',
+        tags_path='selected_tags.csv',
+        **kwargs
+    ) -> None:
+        super().__init__(name)
+        self.model_path = model_path
+        self.tags_path = tags_path
+        self.kwargs = kwargs
+
+    def download(self) -> Tuple[os.PathLike, os.PathLike]:
+        print(f"Loading {self.name} model file from {self.kwargs['repo_id']}")
+
+        model_path = Path(hf_hub_download(
+            **self.kwargs, filename=self.model_path))
+        tags_path = Path(hf_hub_download(
+            **self.kwargs, filename=self.tags_path))
+        return model_path, tags_path
+
+    def load(self) -> None:
+        model_path, tags_path = self.download()
+
+        # only one of these packages should be installed at a time in any one environment
+        # https://onnxruntime.ai/docs/get-started/with-python.html#install-onnx-runtime
+        # TODO: remove old package when the environment changes?
+
+        from onnxruntime import InferenceSession
+
+        # https://onnxruntime.ai/docs/execution-providers/
+        # https://github.com/toriato/stable-diffusion-webui-wd14-tagger/commit/e4ec460122cf674bbf984df30cdb10b4370c1224#r92654958
+        providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+        if tf_device_name == '/cpu:0':
+            providers.pop(0)
+
+        self.model = InferenceSession(str(model_path), providers=providers)
+
+        print(f'Loaded {self.name} model from {model_path}')
+
+        self.tags = pd.read_csv(tags_path)
+
+    def interrogate(
+        self,
+        image: Image
+    ) -> Tuple[
+        Dict[str, float],  # rating confidents
+        Dict[str, float]  # tag confidents
+    ]:
+        # init model
+        if not hasattr(self, 'model') or self.model is None:
+            self.load()
+
+        # code for converting the image and running the model is taken from the link below
+        # thanks, SmilingWolf!
+        # https://huggingface.co/spaces/SmilingWolf/wd-v1-4-tags/blob/main/app.py
+
+        # convert an image to fit the model
+        _, height, _, _ = self.model.get_inputs()[0].shape
+
+        # alpha to white
+        image = image.convert('RGBA')
+        new_image = Image.new('RGBA', image.size, 'WHITE')
+        new_image.paste(image, mask=image)
+        image = new_image.convert('RGB')
+        image = np.asarray(image)
+
+        # PIL RGB to OpenCV BGR
+        image = image[:, :, ::-1]
+        image = make_square(image, height)
+
+        image = smart_resize(image, height)
+        image = image.astype(np.float32)
+        image = np.expand_dims(image, 0)
+
+        # evaluate model
+        input_name = self.model.get_inputs()[0].name
+        label_name = self.model.get_outputs()[0].name
+        confidents = self.model.run([label_name], {input_name: image})[0]
+
+        tags = self.tags[:][['name']]
+        tags['confidents'] = confidents[0]
+
+        # first 4 items are for rating (general, sensitive, questionable, explicit)
+        ratings = dict(tags[:4].values)
+
+        # rest are regular tags
+        tags = dict(tags[4:].values)
+
+        return ratings, tags
+
+
+my_wf =  WaifuDiffusionInterrogator(
+            'wd14-convnextv2-v2',
+            repo_id='SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
+            revision='v2.0'
+        )
+
+def getTags(img_path):
+    global my_wf
+    img = Image.open(img_path)
+    ratings, tags = my_wf.interrogate(img)
+    tags = my_wf.postprocess_tags(tags)
+    
+    return ",".join(tags.keys())
+
+# 定义输入文件夹
+folder_path = os.path.dirname(os.getcwd())
+frame_path = os.path.join(folder_path, "video_frame_w")  # 定义原始图像文件夹
+
+# 轮询开始输出
+frame_files = [f for f in os.listdir(frame_path) if f.endswith(".png")]
+for frame in frame_files:
+    frame_file = os.path.join(frame_path, frame)
+    txt= getTags(frame_file)
+    tag_count= len(txt.split(","))+1
+    # 创建提示词txt文件
+    txt_file = os.path.join(frame_path, f'{os.path.splitext(frame_file)[0]}.txt')
+    with open(txt_file, 'w', encoding='utf-8') as tags:
+        tags.write(txt)
+    print(f'{frame}的提示词反推完成，提取{tag_count}个tag')

75ace0ded9f526b619890babc8ff9869d0e08a8a
huchang47 <huchang47@hotmail.com>
2023-08-20 16:30:12 +0800

    调整文字说明

--- bin/03_CropImage.py
+++ bin/03_CropImage.py
@@ -322,7 +322,7 @@ for file, line in zip(os.listdir(mask_path), lines):
         print("蒙版" + file + "裁切完成！")
 
 # 是否进行下一步
-choice = input("\n是否直接开始下一步，反推提示词？需要启用API后启动SD，并正确安装WD1.4 Tagger插件\n1. 是\n2. 否\n请输入你的选择：")
+choice = input("\n是否直接开始下一步，反推提示词？\n1. 是\n2. 否\n请输入你的选择：")
 if choice == "1":
     subprocess.run(['python', '04_GeneratePrompt.py'])
 else:

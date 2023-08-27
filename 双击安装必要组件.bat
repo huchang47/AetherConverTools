9ae6ba61a3719d899198a0e7b7e206a5a5a1d5c0
huchang47 <huchang47@hotmail.com>
2023-08-28 01:06:02 +0800

    优化环境安装功能

--- 双击安装必要组件.bat
+++ 双击安装必要组件.bat
@@ -1,6 +1,7 @@
 pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install subprocess
 pip3 install pillow
+pip3 uninstall -y torch
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip3 install torchvision
 pip3 install shutil -i

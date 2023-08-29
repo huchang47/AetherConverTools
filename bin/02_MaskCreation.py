38656f34b7587cfc8505c8212cec4b1c7bc25ffa
huchang47 <huchang47@hotmail.com>
2023-08-29 21:58:21 +0800

    开发版本同步到主线

--- bin/02_MaskCreation.py
+++ bin/02_MaskCreation.py
@@ -2,6 +2,9 @@ import subprocess
 import os
 import torch
 import shutil
+import cv2
+import numpy as np
+from PIL import Image
 
 print("检测是否有可用的CUDA设备中……")
 # 检查是否有可用的CUDA设备
@@ -11,6 +14,30 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
     print("加速失败！使用的设备：CPU")
+# 检查cuda的使用接口
+
+
+# 定义寻找最大区域函数
+def max_area(img):
+    # 使用OpenCV 4.x版本的返回值
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # 使用list()创建一个空列表
+    area = list()
+    # 使用enumerate()遍历轮廓列表，并同时获取索引和元素
+    for i, c in enumerate(contours):
+        # 保存计算结果在一个变量中，并复用这个变量
+        area_i = cv2.contourArea(c)
+        area.append(area_i)
+    # 同时获取最大面积的索引和值
+    max_idx, max_area = max(enumerate(area), key=lambda x: x[1])
+
+    # 遍历轮廓列表
+    for k, c in enumerate(contours):
+        # 跳过最大面积的轮廓
+        if k == max_idx: continue
+        # 绘制非最大面积的轮廓为黑色，并填充其内部区域
+        cv2.drawContours(img, contours, k, 0, -1)
+    return img
 
 print("\n请选择蒙版生成算法")
 print("1. 快速，速度快但质量稍差")
@@ -40,28 +67,41 @@ choice2 = input("请输入蒙版算法的编号：")
 if choice2 == '1':
     print("你选择了快速模式")
     print("开始生成蒙版，请注意查看进度。根据图片数量，时间可能很长。\n你可以随时按Ctrl+C停止生成。")
-    subprocess.run(
+    subprocess.call(
         ['transparent-background', '--source', frame_out_dir, '--dest', mask_out_dir, '--type', 'map', '--fast'])
 else:
     print("你选择了标准模式")
     print("开始生成蒙版，请注意查看进度。根据图片数量，时间可能很长。\n你可以随时按Ctrl+C停止生成。")
-    subprocess.run(['transparent-background', '--source', frame_out_dir, '--dest', mask_out_dir, '--type', 'map'])
+    subprocess.call(['transparent-background', '--source', frame_out_dir, '--dest', mask_out_dir, '--type', 'map'])
 
 # 开始修正蒙版名称
 files = sorted(os.listdir(mask_out_dir))
 
-# 遍历文件列表
-for filename in files:
-    if filename.lower().endswith('.png'):
-        file_name, n1 = map(str, filename.split('_'))
-        new_file = f'{file_name}.png'
+# 遍历文件列表  
+for filename in files:  
+    if filename.lower().endswith('.png'):  
+        file_name, n1 = map(str, filename.split('_'))  
+        new_file = f'{file_name}.png'  
+  
+        # 构建文件完整路径  
+        file_path = os.path.join(mask_out_dir, filename)  
+        new_file_path = os.path.join(mask_out_dir, new_file)  
+  
+        # 重命名文件  
+        os.rename(file_path, new_file_path)  
+        im = cv2.imread(new_file_path)  
+        gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  
+        # 二值化  
+        _, im_g = cv2.threshold(gray_image, 177, 255, cv2.THRESH_BINARY)  
+        # 将二值化后的图像转换为numpy数组  
+        im_np = np.array(im_g)  
+        # 调用max_area函数，获取最大面积的图像  
+        im = np.zeros_like(im_np)  # 创建与输入图像大小相同的黑色图像  
+        max_area_mask = max_area(im_np)  # 获取仅包含最大轮廓的掩膜图像  
+        im[max_area_mask == 255] = 255  # 将输出图像中对应的像素设为白色  
+        # 将最大面积的图像写入新的文件  
+        cv2.imwrite(new_file_path, im)  
 
-        # 构建文件完整路径
-        file_path = os.path.join(mask_out_dir, filename)
-        new_file_path = os.path.join(mask_out_dir, new_file)
-
-        # 重命名文件
-        os.rename(file_path, new_file_path)
 
 print("蒙版文件生成完成！")
 
